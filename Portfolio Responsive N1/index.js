63c45e43530c2188bbdda868e61a6ec9aa1ebdf8
WSuarezCristian <125509550+WSuarezCristian@users.noreply.github.com>
2024-05-29 16:46:15 -0300

    Add files via upload

---
+++ Portfolio Responsive N1/index.js
@@ -0,0 +1,7 @@
+let banner = document.querySelector('.banner');
+let menutoggle = document.querySelector('.toggle');
+
+menutoggle.onclick = function() {
+    menutoggle.classList.toggle('active');
+    banner.classList.toggle('active');
+}
